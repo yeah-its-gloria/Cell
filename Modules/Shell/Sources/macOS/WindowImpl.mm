a70f0cae580349a68274ac87c8af06e33a639ab6
yeah-its-gloria <32610623+yeah-its-gloria@users.noreply.github.com>
2024-04-21 02:21:23 -0400

    Implement some keys

--- Modules/Shell/Sources/macOS/WindowImpl.mm
+++ Modules/Shell/Sources/macOS/WindowImpl.mm
@@ -5,7 +5,64 @@
 
 #include <Cell/System/Log.hh>
 
-// TODO: key LUT
+using namespace Cell::Shell;
+
+// TODO: implement more keys
+
+const KeyboardButton KeyLUT[52] = {
+    KeyboardButton::A,
+    KeyboardButton::S,
+    KeyboardButton::D,
+    KeyboardButton::F,
+    KeyboardButton::H,
+    KeyboardButton::G,
+    KeyboardButton::Z,
+    KeyboardButton::X,
+    KeyboardButton::C,
+    KeyboardButton::V,
+    KeyboardButton::None,
+    KeyboardButton::B,
+    KeyboardButton::Q,
+    KeyboardButton::W,
+    KeyboardButton::E,
+    KeyboardButton::R,
+    KeyboardButton::Y,
+    KeyboardButton::T,
+    KeyboardButton::One,
+    KeyboardButton::Two,
+    KeyboardButton::Three,
+    KeyboardButton::Four,
+    KeyboardButton::Five,
+    KeyboardButton::Six,
+    KeyboardButton::None,
+    KeyboardButton::Nine,
+    KeyboardButton::Seven,
+    KeyboardButton::None,
+    KeyboardButton::Eight,
+    KeyboardButton::Zero,
+    KeyboardButton::None,
+    KeyboardButton::O,
+    KeyboardButton::U,
+    KeyboardButton::None,
+    KeyboardButton::I,
+    KeyboardButton::P,
+    KeyboardButton::Return,
+    KeyboardButton::L,
+    KeyboardButton::J,
+    KeyboardButton::None,
+    KeyboardButton::K,
+    KeyboardButton::None,
+    KeyboardButton::None,
+    KeyboardButton::None,
+    KeyboardButton::None,
+    KeyboardButton::N,
+    KeyboardButton::M,
+    KeyboardButton::None,
+    KeyboardButton::Tab,
+    KeyboardButton::Space,
+    KeyboardButton::None,
+    KeyboardButton::Backspace
+};
 
 @implementation CellWindowImpl
 -(BOOL) canBecomeKeyWindow {
@@ -19,20 +76,28 @@
 -(void) keyDown: (NSEvent*) event {
     CELL_ASSERT(event.type == NSEventTypeKeyDown && self.keysRef != nullptr);
 
-    if (event.keyCode != 3) {
+    if (event.keyCode > sizeof(KeyLUT) || KeyLUT[event.keyCode] == KeyboardButton::None) {
+#ifdef _DEBUG
+        const char* identifier = [event.characters cStringUsingEncoding: NSUTF8StringEncoding];
+        Cell::System::Log("Unknown Key Pressed #%; %", event.keyCode, identifier);
+#endif
         return;
     }
 
-    *self.keysRef |= Cell::Shell::KeyboardButton::F;
+    *self.keysRef |= KeyLUT[event.keyCode];
 }
 
 -(void) keyUp: (NSEvent*) event {
     CELL_ASSERT(event.type == NSEventTypeKeyUp && self.keysRef != nullptr);
 
-    if (event.keyCode != 3) {
+    if (event.keyCode > sizeof(KeyLUT) || KeyLUT[event.keyCode] == KeyboardButton::None) {
+#ifdef _DEBUG
+        const char* identifier = [event.characters cStringUsingEncoding: NSUTF8StringEncoding];
+        Cell::System::Log("Unknown Key Released #%; %", event.keyCode, identifier);
+#endif
         return;
     }
 
-    *self.keysRef ^= Cell::Shell::KeyboardButton::F;
+    *self.keysRef ^= KeyLUT[event.keyCode];
 }
 @end
